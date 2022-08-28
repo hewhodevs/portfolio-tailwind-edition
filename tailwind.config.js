435e5406f6cd79c7f26ba2dbc43d17936aec3963
hewhodevs <hewhodevs@outlook.com>
2022-08-29 05:32:30 +0930

    removed unused flowbite dependecy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   plugins: [
-    require('flowbite/plugin')
   ],
   content: [
-    "*.{html,js}", 
-    "./node_modules/flowbite/**/*.js"
+    "*.{html,js}"
   ],
   theme: {
     extend: {
